17edb69e5723ae0d3d752c8ed8d5deaa4abc7d02
Katlego2812 <Katlegomaswadzi@gmail.com>
2024-02-27 22:45:43 +0200

    Removed code from index.html and Added it to javascript

--- src/index.js
+++ src/index.js
@@ -43,3 +43,27 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function displayForecast() {
+  let days = ["Tues", "Wed", "Thurs", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class= "weather-forecast-day>
+    <div class="weather-forecast-date>${day}</div>
+    <div class="weather-forecast-icon">⛅</div>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature"> <strong>20°</strong> </div>
+    <div class="weather-forecast-temperature">10°</div>
+    </div>
+    </div>  
+    `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
