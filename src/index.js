bf1fee534f25f71e95741f1b0d535daf87d93276
Katlego2812 <Katlegomaswadzi@gmail.com>
2024-03-05 22:50:20 +0200

    updated the styles.css and the index.js

--- src/index.js
+++ src/index.js
@@ -59,8 +59,6 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
